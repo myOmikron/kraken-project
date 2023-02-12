dbbf237572a3072261c3e7045514e1fa3cdbea11
myOmikron <git@omikron.dev>
2023-02-12 19:08:49 +0100

    Added configuration for webauthn and sessions

--- kraken/src/api/server.rs
+++ kraken/src/api/server.rs
@@ -1,20 +1,60 @@
 use std::fmt::{Display, Formatter};
 use std::io;
 
-use actix_toolbox::tb_middleware::{setup_logging_mw, LoggingMiddlewareConfig};
+use actix_toolbox::tb_middleware::{
+    setup_logging_mw, DBSessionStore, LoggingMiddlewareConfig, PersistentSession, SessionMiddleware,
+};
+use actix_web::cookie::time::Duration;
+use actix_web::cookie::{Key, KeyError};
+use actix_web::middleware::Compress;
 use actix_web::web::{Data, JsonConfig, PayloadConfig};
 use actix_web::{App, HttpServer};
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
 use rorm::Database;
+use webauthn_rs::prelude::{Url, WebauthnError};
+use webauthn_rs::WebauthnBuilder;
 
 use crate::config::Config;
 
+const ORIGIN_NAME: &str = "Kraken";
+
 pub(crate) async fn start_server(db: Database, config: &Config) -> Result<(), StartServerError> {
+    let key = Key::try_from(
+        BASE64_STANDARD
+            .decode(&config.server.secret_key)?
+            .as_slice(),
+    )?;
+
+    let rp_origin =
+        Url::parse(&config.server.origin_uri).map_err(|_| StartServerError::InvalidOrigin)?;
+
+    let webauthn = Data::new(
+        WebauthnBuilder::new(
+            rp_origin.domain().ok_or(StartServerError::InvalidOrigin)?,
+            &rp_origin,
+        )
+        .unwrap()
+        .rp_name(ORIGIN_NAME)
+        .build()?,
+    );
+
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(db.clone()))
             .app_data(JsonConfig::default())
             .app_data(PayloadConfig::default())
+            .app_data(webauthn.clone())
             .wrap(setup_logging_mw(LoggingMiddlewareConfig::default()))
+            .wrap(
+                SessionMiddleware::builder(DBSessionStore::new(db.clone()), key.clone())
+                    .session_lifecycle(PersistentSession::session_ttl(
+                        PersistentSession::default(),
+                        Duration::hours(1),
+                    ))
+                    .build(),
+            )
+            .wrap(Compress::default())
     })
     .bind((
         config.server.api_listen_address.as_str(),
@@ -29,12 +69,24 @@ pub(crate) async fn start_server(db: Database, config: &Config) -> Result<(), St
 #[derive(Debug)]
 pub(crate) enum StartServerError {
     IO(io::Error),
+    Webauthn(WebauthnError),
+    InvalidSecretKey,
+    InvalidOrigin,
 }
 
 impl Display for StartServerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             StartServerError::IO(err) => write!(f, "Error starting server: {err}"),
+            StartServerError::Webauthn(err) => {
+                write!(f, "Error while constructing Webauthn: {err}")
+            }
+            StartServerError::InvalidSecretKey => write!(
+                f,
+                "Invalid parameter SecretKey.\
+                    Consider using the subcommand keygen and update your configuration file"
+            ),
+            StartServerError::InvalidOrigin => write!(f, "invalid origin specified"),
         }
     }
 }
@@ -42,6 +94,24 @@ impl Display for StartServerError {
 impl From<io::Error> for StartServerError {
     fn from(value: io::Error) -> Self {
         Self::IO(value)
+    }
+}
+
+impl From<base64::DecodeError> for StartServerError {
+    fn from(_value: base64::DecodeError) -> Self {
+        Self::InvalidSecretKey
+    }
+}
+
+impl From<WebauthnError> for StartServerError {
+    fn from(value: WebauthnError) -> Self {
+        Self::Webauthn(value)
+    }
+}
+
+impl From<KeyError> for StartServerError {
+    fn from(_value: KeyError) -> Self {
+        Self::InvalidSecretKey
     }
 }
 
