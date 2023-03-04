bb3d45591b9e952dcbff8e4fb5a678ebc3100a0d
myOmikron <git@omikron.dev>
2023-03-04 20:39:19 +0100

    Added create-admin-user command

--- kraken/src/main.rs
+++ kraken/src/main.rs
@@ -16,16 +16,24 @@
 )]
 
 use std::fs::read_to_string;
+use std::io;
+use std::io::Write;
+use std::process::exit;
 
 use actix_toolbox::logging::setup_logging;
 use actix_web::cookie::Key;
+use argon2::password_hash::SaltString;
+use argon2::{Argon2, PasswordHasher};
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
 use clap::{Parser, Subcommand};
-use rorm::{Database, DatabaseConfiguration, DatabaseDriver};
+use rand::thread_rng;
+use rorm::{insert, query, Database, DatabaseConfiguration, DatabaseDriver, Model};
+use webauthn_rs::prelude::Uuid;
 
 use crate::api::server;
 use crate::config::Config;
+use crate::models::{User, UserInsert};
 
 mod api;
 pub(crate) mod chan;
@@ -40,6 +48,8 @@ enum Command {
     Start,
     /// Generate a secret key
     Keygen,
+    /// Creates a new user with administrative privileges
+    CreateAdminUser,
 }
 
 #[derive(Parser)]
@@ -80,7 +90,68 @@ async fn main() -> Result<(), String> {
             let key = Key::generate();
             println!("{}", BASE64_STANDARD.encode(key.master()));
         }
+        Command::CreateAdminUser => {
+            let db = get_db(&config).await?;
+
+            create_user(db).await?;
+        }
     }
+
+    Ok(())
+}
+
+/// Creates a new admin user
+///
+/// **Parameter**:
+/// - `db`: [Database]
+async fn create_user(db: Database) -> Result<(), String> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut username = String::new();
+    let mut display_name = String::new();
+
+    print!("Enter a username: ");
+    stdout.flush().unwrap();
+    stdin.read_line(&mut username).unwrap();
+    let username = username.trim();
+
+    if query!(&db, (User::F.username,))
+        .condition(User::F.username.equals(username))
+        .optional()
+        .await
+        .unwrap()
+        .is_some()
+    {
+        eprintln!("There is already a user with that name");
+        exit(1);
+    }
+
+    print!("Enter a display name: ");
+    stdout.flush().unwrap();
+    stdin.read_line(&mut display_name).unwrap();
+
+    let password = rpassword::prompt_password("Enter password: ").unwrap();
+
+    let salt = SaltString::generate(&mut thread_rng());
+    let hashed_password = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+
+    insert!(&db, UserInsert)
+        .single(&UserInsert {
+            username: username.to_string(),
+            display_name: display_name.to_string(),
+            password_hash: hashed_password,
+            admin: true,
+            last_login: None,
+            uuid: Uuid::new_v4().into_bytes().to_vec(),
+        })
+        .await
+        .map_err(|e| format!("Failed to create user: {e}"))?;
+
+    println!("Created user {username}");
 
     Ok(())
 }
