94bfaa3ac6095d590d3b8ff6ccec0d0bea5b026f
gammelalf <gammelalf@hopfen.space>
2023-03-04 22:25:23 +0100

    Used OpenAPI-Generator and wrapped it

--- kraken_frontend/src/api/api.ts
+++ kraken_frontend/src/api/api.ts
@@ -1,11 +1,87 @@
 import { login, test, registerKey, authenticate, logout } from "./auth";
+import { handleError } from "./error";
+import {
+    BruteforceSubdomainsRequest,
+    CreateLeechRequest,
+    CreateUserRequest,
+    CreateWorkspaceRequest,
+    QueryCertificateTransparencyRequest,
+    ScanTcpPortsRequest,
+    UpdateLeechRequest,
+    UpdateMeRequest,
+} from "./generated/models";
+import { Configuration } from "./generated/runtime";
+import {
+    AdminWorkspacesApi,
+    AttacksApi,
+    LeechManagementApi,
+    UserAdminManagementApi,
+    UserManagementApi,
+    WorkspacesApi,
+} from "./generated/apis";
+
+/** Database id i.e. and u32 */
+export type ID = number;
+
+const configuration = new Configuration({
+    basePath: window.location.origin,
+});
+const userAdminManagement = new UserAdminManagementApi(configuration);
+const adminWorkspaces = new AdminWorkspacesApi(configuration);
+const attacks = new AttacksApi(configuration);
+// const authentication = new generated.AuthenticationApi(configuration);
+const leechManagement = new LeechManagementApi(configuration);
+const userManagement = new UserManagementApi(configuration);
+const workspaces = new WorkspacesApi(configuration);
 
 export const Api = {
+    admin: {
+        users: {
+            all: () => handleError(userAdminManagement.getAllUsers()),
+            create: (user: CreateUserRequest) =>
+                handleError(userAdminManagement.createUser({ createUserRequest: user })),
+            get: (username: string) => handleError(userAdminManagement.getUser({ username })),
+            delete: (username: string) => handleError(userAdminManagement.deleteUser({ username })),
+        },
+        workspaces: {
+            all: () => handleError(adminWorkspaces.getAllWorkspacesAdmin()),
+            get: (id: ID) => handleError(adminWorkspaces.getWorkspaceAdmin({ id })),
+        },
+    },
+    leeches: {
+        all: () => handleError(leechManagement.getAllLeeches()),
+        create: (leech: CreateLeechRequest) => handleError(leechManagement.createLeech({ createLeechRequest: leech })),
+        get: (id: ID) => handleError(leechManagement.getLeech({ id })),
+        update: (id: ID, leech: UpdateLeechRequest) =>
+            handleError(leechManagement.updateLeech({ id, updateLeechRequest: leech })),
+        delete: (id: ID) => handleError(leechManagement.deleteLeech({ id })),
+    },
+    attacks: {
+        bruteforceSubdomains: (attack: BruteforceSubdomainsRequest) =>
+            handleError(attacks.bruteforceSubdomains({ bruteforceSubdomainsRequest: attack })),
+        queryCertificateTransparency: (attack: QueryCertificateTransparencyRequest) =>
+            handleError(attacks.queryCertificateTransparency({ queryCertificateTransparencyRequest: attack })),
+        scanTcpPorts: (attack: ScanTcpPortsRequest) =>
+            handleError(attacks.scanTcpPorts({ scanTcpPortsRequest: attack })),
+    },
     auth: {
         login,
         logout,
         test,
         registerKey,
         authenticate,
+    },
+    user: {
+        get: () => handleError(userManagement.getMe()),
+        update: (user: UpdateMeRequest) => handleError(userManagement.updateMe({ updateMeRequest: user })),
+        setPassword: (currentPassword: string, newPassword: string) =>
+            handleError(userManagement.setPassword({ setPasswordRequest: { currentPassword, newPassword } })),
+    },
+    workspaces: {
+        all: () => handleError(workspaces.getAllWorkspaces()),
+        create: (workspace: CreateWorkspaceRequest) =>
+            handleError(workspaces.createWorkspace({ createWorkspaceRequest: workspace })),
+        get: (id: ID) => handleError(workspaces.getWorkspace({ id })),
+        delete: (id: ID) => handleError(workspaces.deleteWorkspace({ id })),
     },
 };
---
+++ kraken_frontend/src/api/error.ts
@@ -0,0 +1,58 @@
+import { Err, Ok, Result } from "../utils/result";
+
+export enum StatusCode {
+    ArbitraryJSError = -2,
+    JsonDecodeError = -1,
+
+    LoginFailed = 1000,
+    NotFound = 1001,
+    InvalidContentType = 1002,
+    InvalidJson = 1003,
+    PayloadOverflow = 1004,
+    Unauthenticated = 1005,
+    Missing2fa = 1006,
+    MissingPrivileges = 1007,
+    InternalServerError = 2000,
+    DatabaseError = 2001,
+    SessionError = 2002,
+}
+
+export type ApiError = {
+    status_code: StatusCode;
+    message: string;
+};
+
+/**
+ * Wraps a promise returned by the generated SDK which handles its errors and returns a {@link Result}
+ */
+export async function handleError<T>(promise: Promise<T>): Promise<Result<T, ApiError>> {
+    try {
+        return Ok(await promise);
+    } catch (e) {
+        if (e instanceof Response) {
+            return Err(await parseError(e));
+        } else {
+            return Err({
+                status_code: StatusCode.ArbitraryJSError,
+                message: "The server's response was invalid json",
+            });
+        }
+    }
+}
+
+/**
+ * Parse a response's body into an {@link ApiError}
+ *
+ * This function assumes but doesn't check, that the response is an error.
+ */
+export async function parseError(response: Response): Promise<ApiError> {
+    try {
+        return await response.json();
+    } catch {
+        console.error("Got invalid json", response.body);
+        return {
+            status_code: StatusCode.JsonDecodeError,
+            message: "The server's response was invalid json",
+        };
+    }
+}
