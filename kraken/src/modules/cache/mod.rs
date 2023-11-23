4d273e490ead1f959acd5067b22787df82c8fd35
myOmikron <git@omikron.dev>
2023-11-23 16:52:30 +0100

    Reworked cache to allow refreshing

--- kraken/src/modules/cache/mod.rs
+++ kraken/src/modules/cache/mod.rs
@@ -7,16 +7,14 @@ use std::sync::RwLock;
 use chrono::{DateTime, Duration, Utc};
 use futures::TryStreamExt;
 use rorm::{query, FieldAccess, Model};
-use thiserror::Error;
 use uuid::Uuid;
 
-use crate::api::handler::ApiError;
 use crate::chan::GLOBAL;
 use crate::models::{Workspace, WorkspaceMember};
 
 struct WorkspaceUsers {
     last_refresh: DateTime<Utc>,
-    users: Vec<Uuid>,
+    users: Option<Vec<Uuid>>,
 }
 
 /// The cache for accessing the users that have access to a workspace
@@ -29,15 +27,67 @@ impl WorkspaceCache {
     /// Create a new workspace cache
     pub fn new() -> Self {
         Self {
-            cache: RwLock::new(HashMap::new()),
+            cache: Default::default(),
         }
     }
 
-    /// Retrieves all users that have access to the workspace
-    pub async fn get_users(&self, workspace: Uuid) -> Result<Vec<Uuid>, CacheError> {
-        let now = Utc::now();
+    /// Trigger a manual refresh of the users of a specific workspace
+    pub async fn refresh_users(&self, workspace: Uuid) -> Result<Option<Vec<Uuid>>, rorm::Error> {
+        {
+            let guard = self.cache.read().expect(
+                "If you ever encounter this error, please open an issue with the stacktrace",
+            );
 
-        let mut users: Vec<Uuid> = vec![];
+            if !guard.contains_key(&workspace) {
+                return Ok(None);
+            }
+        }
+
+        let now = Utc::now();
+        let mut users = None;
+
+        let mut tx = GLOBAL.db.start_transaction().await?;
+
+        let owner = query!(&mut tx, (Workspace::F.owner,))
+            .condition(Workspace::F.uuid.equals(workspace))
+            .optional()
+            .await?;
+
+        if let Some((owner,)) = owner {
+            let mut members: Vec<Uuid> = query!(&mut tx, (WorkspaceMember::F.member,))
+                .condition(WorkspaceMember::F.workspace.equals(workspace))
+                .stream()
+                .map_ok(|(x,)| *x.key())
+                .try_collect()
+                .await?;
+
+            members.push(*owner.key());
+            users = Some(members);
+        }
+
+        tx.commit().await?;
+        let mut guard = self
+            .cache
+            .write()
+            .expect("If you ever encounter this error, please open an issue with the stacktrace");
+        guard.entry(workspace).and_modify(|x| {
+            *x = WorkspaceUsers {
+                users: users.clone(),
+                last_refresh: now,
+            }
+        });
+
+        Ok(users)
+    }
+
+    /// Retrieves all users that have access to the workspace
+    ///
+    /// If the Option is None, there is no workspace with that uuid
+    pub async fn get_users(&self, workspace: Uuid) -> Result<Option<Vec<Uuid>>, rorm::Error> {
+        let now = Utc::now();
+        let refresh_period = Duration::minutes(5);
+
+        let mut users: Option<Vec<Uuid>> = None;
         let entry = {
             let guard = self.cache.read().expect(
                 "If you ever encounter this error, please open an issue with the stacktrace",
@@ -48,7 +98,7 @@ impl WorkspaceCache {
                 last_refresh,
             }) = guard.get(&workspace)
             {
-                if now.sub(Duration::minutes(5)) >= *last_refresh {
+                if now.sub(refresh_period) >= *last_refresh {
                     None
                 } else {
                     Some(users.clone())
@@ -59,62 +109,22 @@ impl WorkspaceCache {
         };
 
         // If the key does not exists or the last refresh time
-        // is more than `REFRESH_PERIOD` ago, update the entry
+        // is more than `refresh_period` ago, update the entry
         if entry.is_none() {
-            let mut tx = GLOBAL.db.start_transaction().await?;
-
-            let (owner,) = query!(&mut tx, (Workspace::F.owner,))
-                .condition(Workspace::F.uuid.equals(workspace))
-                .optional()
-                .await?
-                .ok_or(CacheError::WorkspaceNotFound)?;
-
-            let members: Vec<Uuid> = query!(&mut tx, (WorkspaceMember::F.member,))
-                .condition(WorkspaceMember::F.workspace.equals(workspace))
-                .stream()
-                .map_ok(|(x,)| *x.key())
-                .try_collect()
-                .await?;
-
-            tx.commit().await?;
-
-            users.extend(members);
-            users.push(*owner.key());
             {
-                let mut guard = self.cache.write().expect(
+                self.refresh_users(workspace).await?;
+
+                let guard = self.cache.read().expect(
                     "If you ever encounter this error, please open an issue with the stacktrace",
                 );
-                guard.entry(workspace).and_modify(|x| {
-                    *x = WorkspaceUsers {
-                        users: users.clone(),
-                        last_refresh: now,
-                    }
-                });
+                if let Some(u) = guard.get(&workspace) {
+                    users = u.users.clone();
+                }
             }
         } else if let Some(u) = entry {
-            users.extend(u);
+            users = u;
         }
 
         Ok(users)
-    }
-}
-
-/// The errors that can occur while working with the cache
-#[derive(Error, Debug)]
-pub enum CacheError {
-    /// Database error occurred
-    #[error("Database error: {0}")]
-    Database(#[from] rorm::Error),
-    /// Workspace was not found
-    #[error("Workspace was not found")]
-    WorkspaceNotFound,
-}
-
-impl From<CacheError> for ApiError {
-    fn from(value: CacheError) -> Self {
-        match value {
-            CacheError::Database(x) => ApiError::DatabaseError(x),
-            CacheError::WorkspaceNotFound => ApiError::InvalidWorkspace,
-        }
     }
 }
