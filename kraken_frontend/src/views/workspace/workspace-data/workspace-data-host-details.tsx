69d1da21050f1e1d523c2defd5ef85b3b7745856
mizcky <mhutter@trufflepig-forensics.de>
2023-12-05 16:16:36 +0100

    added tab selection to the detail views

--- kraken_frontend/src/views/workspace/workspace-data/workspace-data-host-details.tsx
+++ kraken_frontend/src/views/workspace/workspace-data/workspace-data-host-details.tsx
@@ -6,14 +6,18 @@ import Textarea from "../../../components/textarea";
 import { toast } from "react-toastify";
 import EditableTags from "../components/editable-tags";
 import { WORKSPACE_CONTEXT } from "../workspace";
+import { WorkspaceDataDomainDetails } from "./workspace-data-domain-details";
+import { WorkspaceDataPortDetails } from "./workspace-data-port-details";
+import { WorkspaceDataServiceDetails } from "./workspace-data-service-details";
 
 export type WorkspaceDataHostDetailsProps = {
     host: string;
     updateHost?: (uuid: string, update: Partial<FullHost>) => void;
+    tab: "general" | "results" | "relations";
 };
 
 export function WorkspaceDataHostDetails(props: WorkspaceDataHostDetailsProps) {
-    const { host: uuid, updateHost: signalUpdate } = props;
+    const { host: uuid, updateHost: signalUpdate, tab: tab } = props;
     const {
         workspace: { uuid: workspace },
     } = React.useContext(WORKSPACE_CONTEXT);
@@ -37,38 +41,44 @@ export function WorkspaceDataHostDetails(props: WorkspaceDataHostDetailsProps) {
                 handleApiError(() => {
                     if (msg !== undefined) toast.success(msg);
                     if (signalUpdate !== undefined) signalUpdate(uuid, update);
-                }),
+                })
             );
     }
 
     if (host === null) return null;
     return (
         <>
-            <div className={"pane"}>
-                <h3 className={"sub-heading"}>Host</h3>
-                {host.ipAddr}
-            </div>
-            <div className={"pane"}>
-                <h3 className={"sub-heading"}>Comment</h3>
-                <Textarea value={host.comment} onChange={(comment) => setHost({ ...host, comment })} />
-                <button
-                    className={"button"}
-                    onClick={() => host && update(host.uuid, { comment: host.comment }, "Updated comment")}
-                >
-                    Update
-                </button>
-            </div>
-            <div className={"pane"}>
-                <h3 className={"sub-heading"}>Tags</h3>
-                <EditableTags
-                    workspace={workspace}
-                    tags={host.tags}
-                    onChange={(tags) => {
-                        setHost((host) => host && { ...host, tags });
-                        update(host.uuid, { tags });
-                    }}
-                />
-            </div>
+            {tab === "general" ? (
+                <>
+                    <div className={"pane"}>
+                        <h3 className={"sub-heading"}>Host</h3>
+                        {host.ipAddr}
+                    </div>
+                    <div className={"pane"}>
+                        <h3 className={"sub-heading"}>Comment</h3>
+                        <Textarea value={host.comment} onChange={(comment) => setHost({ ...host, comment })} />
+                        <button
+                            className={"button"}
+                            onClick={() => host && update(host.uuid, { comment: host.comment }, "Updated comment")}
+                        >
+                            Update
+                        </button>
+                    </div>
+                    <div className={"pane"}>
+                        <h3 className={"sub-heading"}>Tags</h3>
+                        <EditableTags
+                            workspace={workspace}
+                            tags={host.tags}
+                            onChange={(tags) => {
+                                setHost((host) => host && { ...host, tags });
+                                update(host.uuid, { tags });
+                            }}
+                        />
+                    </div>
+                </>
+            ) : (
+                <>{tab === "results" ? <div> host results</div> : <div> hosts relations</div>}</>
+            )}
         </>
     );
 }
