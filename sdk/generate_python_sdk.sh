c4485f1d242b3cbf5d981d704fad2fea025fb054
myOmikron <git@omikron.dev>
2023-12-06 12:17:48 +0100

    Removed useless tests

--- sdk/generate_python_sdk.sh
+++ sdk/generate_python_sdk.sh
@@ -4,12 +4,17 @@ set -e
 
 PACKAGE_VERSION="0.1.0"
 
+rm -rf python-sdk/*
+rm -rf python-sdk/.*
+
 npx @openapitools/openapi-generator-cli generate -i "../kraken_frontend/openapi.json" \
   -g "python" \
   -o "./python-sdk/" \
   --git-repo-id="kraken-project" \
   --git-user-id="myOmikron" \
   --additional-properties="library=asyncio,projectName=kraken-sdk,packageName=kraken_sdk,packageVersion=$PACKAGE_VERSION"
+
+rm -rf "./python-sdk/test"
 
 rm -rf "./python-sdk/.github"
 
