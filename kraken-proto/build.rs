ddfb4825b6dd266abe76998242629136978f7fd0
Jan Jurzitza <gh@webfreak.org>
2024-02-15 14:33:22 +0100

    fix build

--- kraken-proto/build.rs
+++ kraken-proto/build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile(&["./proto/attacks.proto"], &["./proto/"])?;
+    tonic_build::configure()
+        // Required for protoc versions < 3.15
+        // 3.15 stabilized the feature and enabled it by default
+        // This was relevant for the CI
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["./proto/attacks.proto"], &["./proto/"])?;
     Ok(())
 }
