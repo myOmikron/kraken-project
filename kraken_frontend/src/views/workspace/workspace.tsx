9a3804722f96bbc6b26271da64683999b4f5bad2
myOmikron <git@omikron.dev>
2024-03-11 14:24:36 +0100

    Added notes to workspace context

--- kraken_frontend/src/views/workspace/workspace.tsx
+++ kraken_frontend/src/views/workspace/workspace.tsx
@@ -16,6 +16,7 @@ export const WORKSPACE_CONTEXT = React.createContext<WorkspaceContext>({
         owner: { uuid: "", username: "", displayName: "" },
         attacks: [],
         members: [],
+        notes: "",
         createdAt: new Date(),
     },
 });
