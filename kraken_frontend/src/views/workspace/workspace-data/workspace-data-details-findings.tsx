f9a40b95934bc729a44ab5d800f910214f956ff8
gammelalf <gammelalf@hopfen.space>
2024-03-18 19:55:23 +0100

    Made details tab link to findings

--- kraken_frontend/src/views/workspace/workspace-data/workspace-data-details-findings.tsx
+++ kraken_frontend/src/views/workspace/workspace-data/workspace-data-details-findings.tsx
@@ -1,7 +1,13 @@
 import { ListFindings } from "../../../api/generated";
 import SeverityIcon from "../../../svg/severity";
+import { ROUTES } from "../../../routes";
+import { WORKSPACE_CONTEXT } from "../workspace";
+import React from "react";
 
 export default function WorkspaceDataDetailsFindings({ findings }: { findings: ListFindings | null }) {
+    const {
+        workspace: { uuid: workspace },
+    } = React.useContext(WORKSPACE_CONTEXT);
     return (
         <div className="workspace-data-details-relations-container">
             <div className="workspace-data-details-relations-header workspace-data-details-findings">
@@ -12,7 +18,10 @@ export default function WorkspaceDataDetailsFindings({ findings }: { findings: L
             {findings ? (
                 <div className="workspace-data-details-relations-body workspace-data-details-findings">
                     {findings.findings.map((r) => (
-                        <div className="workspace-data-details-relations-entry">
+                        <div
+                            className="workspace-data-details-relations-entry"
+                            {...ROUTES.WORKSPACE_FINDINGS_EDIT.clickHandler({ wUuid: workspace, fUuid: r.uuid })}
+                        >
                             <SeverityIcon severity={r.severity} />
                             <span>{r.cve}</span>
                             <span>{r.name}</span>
