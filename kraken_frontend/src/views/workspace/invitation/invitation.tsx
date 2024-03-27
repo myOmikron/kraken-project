d90e471b6fa5541db2049ca631e504f96b20d413
Jan Jurzitza <gh@webfreak.org>
2024-03-27 19:52:44 +0100

    invitation class to functional

--- kraken_frontend/src/views/workspace/invitation/invitation.tsx
+++ kraken_frontend/src/views/workspace/invitation/invitation.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Popup from "reactjs-popup";
 import { Api, UUID } from "../../../api/api";
 import { SimpleUser, SimpleWorkspace } from "../../../api/generated";
@@ -12,63 +11,48 @@ type InvitationProps = {
     from: SimpleUser;
     onFinish(): void;
 };
-type InvitationState = {};
 
-export default class Invitation extends React.Component<InvitationProps, InvitationState> {
-    constructor(props: InvitationProps) {
-        super(props);
-
-        this.state = {};
+export default function Invitation(props: InvitationProps) {
+    function acceptInvitation() {
+        return Api.invitations.accept(props.invitationUuid).then(handleApiError(() => props.onFinish()));
     }
 
-    componentDidUpdate(prevProps: Readonly<InvitationProps>) {
-        if (prevProps !== this.props) {
-            this.setState({ invitationPopup: true });
-        }
+    function declineInvitation() {
+        return Api.invitations.decline(props.invitationUuid).then(handleApiError(() => props.onFinish()));
     }
 
-    async acceptInvitation() {
-        await Api.invitations.accept(this.props.invitationUuid).then(handleApiError(() => this.props.onFinish()));
-    }
-
-    async declineInvitation() {
-        await Api.invitations.decline(this.props.invitationUuid).then(handleApiError(() => this.props.onFinish()));
-    }
-
-    render() {
-        return (
-            <Popup closeOnEscape={false} closeOnDocumentClick={false} modal={true} nested={true} open={true}>
-                <div className="invitation-popup pane">
-                    <div className="invitation-icon">
-                        <WorkspaceIcon />
-                    </div>
-                    <h2 className="sub-heading">Workspace invitation</h2>
-                    <span className="invitation-text">
-                        <span>
-                            {this.props.from.displayName} ({this.props.from.username}) invited you to join
-                        </span>
-                        <span>"{this.props.workspace.name}"</span>
-                    </span>
-                    <button
-                        className="invitation-button  button"
-                        onClick={async (e) => {
-                            e.preventDefault();
-                            await this.acceptInvitation();
-                        }}
-                    >
-                        Accept
-                    </button>
-                    <button
-                        className="invitation-button button"
-                        onClick={async (e) => {
-                            e.preventDefault();
-                            await this.declineInvitation();
-                        }}
-                    >
-                        Decline
-                    </button>
+    return (
+        <Popup closeOnEscape={false} closeOnDocumentClick={false} modal nested open>
+            <div className="invitation-popup pane">
+                <div className="invitation-icon">
+                    <WorkspaceIcon />
                 </div>
-            </Popup>
-        );
-    }
+                <h2 className="sub-heading">Workspace invitation</h2>
+                <span className="invitation-text">
+                    <span>
+                        {props.from.displayName} ({props.from.username}) invited you to join
+                    </span>
+                    <span>"{props.workspace.name}"</span>
+                </span>
+                <button
+                    className="invitation-button button"
+                    onClick={async (e) => {
+                        e.preventDefault();
+                        await acceptInvitation();
+                    }}
+                >
+                    Accept
+                </button>
+                <button
+                    className="invitation-button button"
+                    onClick={async (e) => {
+                        e.preventDefault();
+                        await declineInvitation();
+                    }}
+                >
+                    Decline
+                </button>
+            </div>
+        </Popup>
+    );
 }
