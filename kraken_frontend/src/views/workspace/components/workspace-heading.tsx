5e82e696bc546ec158b8744e1f0293473e85b73a
Jan Jurzitza <gh@webfreak.org>
2024-03-27 19:55:26 +0100

    WorkspaceHeading class to functional

--- kraken_frontend/src/views/workspace/components/workspace-heading.tsx
+++ kraken_frontend/src/views/workspace/components/workspace-heading.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Select, { components } from "react-select";
 import { Api, UUID } from "../../../api/api";
 import { SimpleWorkspace } from "../../../api/generated";
@@ -12,84 +12,65 @@ type WorkspaceHeadingProps = {
     uuid: UUID;
     name: string;
 };
-type WorkspaceHeadingState = {
-    workspaces?: Array<SimpleWorkspace>;
-};
 
-export default class WorkspaceHeading extends React.Component<WorkspaceHeadingProps, WorkspaceHeadingState> {
-    constructor(props: WorkspaceHeadingProps) {
-        super(props);
+export default function WorkspaceHeading(props: WorkspaceHeadingProps) {
+    const [workspaces, setWorkspaces] = React.useState<Array<SimpleWorkspace>>([]);
 
-        this.state = {};
-    }
+    useEffect(() => {
+        Api.workspaces.all().then(handleApiError(({ workspaces }) => setWorkspaces(workspaces)));
+    });
 
-    componentDidMount() {
-        this.fetchState();
-    }
-
-    fetchState() {
-        Api.workspaces.all().then(
-            handleApiError(({ workspaces }) =>
-                this.setState({
-                    workspaces,
-                }),
-            ),
-        );
-    }
-
-    render() {
-        return (
-            <div className={"pane workspace-heading"}>
-                <Select<{ label: string; value: string }>
-                    className={"dropdown"}
-                    components={{
-                        Option: (props) => (
-                            <div
-                                onMouseDown={(e) => {
-                                    if ((e.ctrlKey && e.button == 0) || e.button == 1) {
-                                        e.stopPropagation();
-                                        ROUTES.WORKSPACE_HOSTS.open({ uuid: props.data.value });
-                                    }
-                                }}
-                            >
-                                <components.Option {...props} />
-                            </div>
-                        ),
+    return (
+        <div className={"pane workspace-heading"}>
+            <Select<{ label: string; value: string }>
+                className={"dropdown"}
+                components={{
+                    Option: (props) => (
+                        <div
+                            onMouseDown={(e) => {
+                                if ((e.ctrlKey && e.button == 0) || e.button == 1) {
+                                    e.stopPropagation();
+                                    ROUTES.WORKSPACE_HOSTS.open({ uuid: props.data.value });
+                                }
+                            }}
+                        >
+                            <components.Option {...props} />
+                        </div>
+                    ),
+                }}
+                onChange={(t) => {
+                    if (!t) return;
+                    ROUTES.WORKSPACE_HOSTS.visit({ uuid: t.value });
+                }}
+                options={
+                    workspaces
+                        ?.filter((e) => {
+                            return e.uuid !== props.uuid;
+                        })
+                        .map((w) => ({
+                            label: w.name,
+                            value: w.uuid,
+                        })) ?? []
+                }
+                value={{
+                    label: props.name,
+                    value: props.uuid,
+                }}
+                isClearable={false}
+                autoFocus={false}
+                styles={clearSelectStyles()}
+            />
+            <div className={"workspace-heading-uuid"}>
+                {props.uuid}
+                <button
+                    className={"icon-button"}
+                    onClick={async () => {
+                        await copyToClipboard(props.uuid);
                     }}
-                    onChange={(t) => {
-                        if (!t) return;
-                        ROUTES.WORKSPACE_HOSTS.visit({ uuid: t.value });
-                    }}
-                    options={
-                        this.state.workspaces
-                            ?.filter((e) => {
-                                return e.uuid !== this.props.uuid;
-                            })
-                            .map((w) => ({
-                                label: w.name,
-                                value: w.uuid,
-                            })) ?? []
-                    }
-                    value={{
-                        label: this.props.name,
-                        value: this.props.uuid,
-                    }}
-                    isClearable={false}
-                    autoFocus={false}
-                    styles={clearSelectStyles()}
-                />
-                <div className={"workspace-heading-uuid"}>
-                    {this.props.uuid}
-                    <button
-                        className={"icon-button"}
-                        onClick={async () => {
-                            await copyToClipboard(this.props.uuid);
-                        }}
-                    >
-                        <CopyIcon />
-                    </button>
-                </div>
+                >
+                    <CopyIcon />
+                </button>
             </div>
-        );
-    }
+        </div>
+    );
 }
