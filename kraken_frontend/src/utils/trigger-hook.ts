28ad81dcbd0c1244c54fa2a2951e886efd87696a
mizcky <mhutter@trufflepig-forensics.de>
2024-03-27 20:15:17 +0100

    added trigger update hook

---
+++ kraken_frontend/src/utils/trigger-hook.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * This hook simply returns a function which, when called, will trigger a rerender.
+ *
+ * @returns function that can be called to trigger rerender
+ */
+export function useTriggerUpdate() {
+    const [_, setDummy] = React.useState({});
+    return () => setDummy({});
+}
